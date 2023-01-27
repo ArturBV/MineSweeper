7458d36e09489a901514f0a776255cbda057eb01
ArturBV <yablokco44@gmail.com>
2023-01-27 19:09:05 +0300

    made more convinient layouts

--- field.py
+++ field.py
@@ -1,5 +1,14 @@
 import sys
-from PyQt5.QtWidgets import QPushButton, QWidget, QApplication, QDesktopWidget, QGridLayout, QLabel
+from PyQt5.QtWidgets import (
+    QPushButton,
+    QWidget,
+    QApplication,
+    QDesktopWidget,
+    QGridLayout,
+    QHBoxLayout,
+    QVBoxLayout,
+    QLabel
+    )
 from PyQt5.QtCore import Qt
 from Game import Generator
 
@@ -16,29 +25,37 @@ class Field(QWidget):
     def initUI(self):
         self.resize(*start_resolution)
 
-        grid = QGridLayout()
-        self.setLayout(grid)
-
-        # 3 for counter, smile and timer(?)
-        top_bar_positions = self.field.get_top_pos(self.top_bar_n)
-        # top_bar_counter = QLabel("0")
-        # top_bar_smile = QLabel("^_^")
-        # top_bar_timer = QLabel("TODO timer")
-        top_bar_widgets = [QLabel("Mines left: 0"), QLabel("^_^"), QLabel("TODO timer")]
-        for widget, pos in zip(top_bar_widgets, top_bar_positions):
-            widget.setAlignment(Qt.AlignVCenter | Qt.AlignTop)
-            grid.addWidget(widget, *pos)
-
-        btn_positions = [(i + 1, j) for i in range(self.field.size) for j in range(self.field.size)]
-        btn_widgets = [QPushButton(f"{i}", self) for i in range(self.field.size * self.field.size)]
-        for btn, pos in zip(btn_widgets, btn_positions):
-            btn.setMinimumSize(start_resolution[1] // self.field.size, start_resolution[1] // self.field.size)
-            # btn.setMaximumSize(start_resolution[1] // self.field.size, start_resolution[1] // self.field.size)
-            grid.addWidget(btn, *pos)
+        self.gamefield_layout = QGridLayout()
+        self.info_layout = QHBoxLayout()
+        pagelayout = QVBoxLayout()
+        
+        pagelayout.addLayout(self.info_layout)
+        pagelayout.addLayout(self.gamefield_layout)
+        
+        self.set_info_layout()
+        self.set_gamelayout()
+        self.setLayout(pagelayout)
 
         self.center_window()
         self.setWindowTitle("MineSweeper by ArturBV")
         self.show()
+
+    def set_info_layout(self):
+        # top_bar_positions = self.field.get_top_pos(self.top_bar_n)
+        top_bar_widgets = [QLabel("Mines left: 0"), QLabel("^_^"), QLabel("TODO timer")]
+        for widget in top_bar_widgets:
+            widget.setAlignment(Qt.AlignHCenter | Qt.AlignTop)
+            self.info_layout.addWidget(widget)
+
+    def set_gamelayout(self):
+        # 3 for counter, smile and timer(?)
+
+        btn_positions = [(i, j) for i in range(self.field.size) for j in range(self.field.size)]
+        btn_widgets = [QPushButton(f"{i}", self) for i in range(self.field.size * self.field.size)]
+        for btn, pos in zip(btn_widgets, btn_positions):
+            btn.setMinimumSize(start_resolution[1] // self.field.size, start_resolution[1] // self.field.size)
+            # btn.setMaximumSize(start_resolution[1] // self.field.size, start_resolution[1] // self.field.size)
+            self.gamefield_layout.addWidget(btn, *pos)
 
     def center_window(self):
         w_frame = self.frameGeometry()
